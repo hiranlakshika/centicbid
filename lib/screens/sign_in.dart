50ab947c47cd3b51118cf1deb108da69972184ff
Hiran Lakshika <hiranlakshika@gmail.com>
2021-06-05 15:51:39 +0530

    Sign in UI enhancements

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,9 +1,11 @@
 import 'package:centicbid/screens/reset_password.dart';
-import 'package:centicbid/screens/sign_up.dart';
 import 'package:centicbid/services/auth.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:the_validator/the_validator.dart';
+
+import '../util.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -18,6 +20,9 @@ class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   bool _rememberMe = false;
   bool _loading = false;
+  bool _termsAndConditions = false;
+  bool _isSignup = false;
+  String _appBarTitle = 'Sign in';
 
   Widget _buildEmailTF() {
     return Column(
@@ -82,95 +87,256 @@ class _SignInState extends State<SignIn> {
   }
 
   Widget _buildRememberMeCheckbox() {
-    return Container(
-      height: 20.0,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Theme(
-            data: ThemeData(unselectedWidgetColor: Colors.blue),
-            child: Checkbox(
-              value: _rememberMe,
-              checkColor: Colors.white,
-              onChanged: (value) {
-                setState(() {
-                  _rememberMe = value!;
-                });
-              },
+    return Visibility(
+      visible: !_isSignup,
+      child: Container(
+        height: 20.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Theme(
+              data: ThemeData(unselectedWidgetColor: Colors.blue),
+              child: Checkbox(
+                value: _rememberMe,
+                checkColor: Colors.white,
+                onChanged: (value) {
+                  setState(() {
+                    _rememberMe = value!;
+                  });
+                },
+              ),
+            ),
+            Text(
+              'Remember me',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginBtn() {
+    return Visibility(
+      visible: !_isSignup,
+      child: Container(
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              setState(() => _loading = true);
+              dynamic result = await _auth.signInWithEmail(_email, _password);
+              if (result != null) {
+                print("Signed in successfully");
+              } else {
+                setState(() => _loading = false);
+              }
+            }
+          },
+          child: Text(
+            'Sign in',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 1.5,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          Text(
-            'Remember me',
+        ),
+      ),
+    );
+  }
+
+  Widget _registerBtn() {
+    return Visibility(
+      visible: _isSignup,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 25.0),
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              if (_termsAndConditions) {
+                setState(() => _loading = true);
+                dynamic result =
+                    await _auth.registerWithEmail(_email, _password);
+                if (result != null) {
+                  showErrorToast("Successfully Registered");
+                  setState(() => _loading = false);
+                  Navigator.pop(context);
+                }
+              } else {
+                showErrorToast("You must agree to the terms and conditions");
+              }
+            }
+          },
+          child: Text(
+            'Sign up',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 1.5,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignInBtn() {
+    return Visibility(
+      visible: _isSignup,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RichText(
+            text: TextSpan(
+              text: 'Have an Account? ',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                _isSignup = false;
+                _appBarTitle = 'Sign in';
+              });
+            },
+            child: Text(
+              'Sign In',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildLoginBtn() {
-    return Container(
-      width: double.infinity,
-      child: ElevatedButton(
-        onPressed: () async {
-          if (_formKey.currentState!.validate()) {
-            setState(() => _loading = true);
-            dynamic result = await _auth.signInWithEmail(_email, _password);
-            if (result != null) {
-              print("Signed in successfully");
-            } else {
-              setState(() => _loading = false);
-            }
-          }
-        },
-        child: Text(
-          'Log In',
-          style: TextStyle(
-            color: Colors.white,
-            letterSpacing: 1.5,
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-
   Widget _buildForgotPasswordBtn() {
-    return Container(
-      alignment: Alignment.center,
-      child: TextButton(
-        onPressed: () => Get.to(() => ResetPassword()),
-        child: Text(
-          'Forgot Password?',
+    return Visibility(
+      visible: !_isSignup,
+      child: Container(
+        alignment: Alignment.center,
+        child: TextButton(
+          onPressed: () => Get.to(() => ResetPassword()),
+          child: Text(
+            'Forgot Password?',
+          ),
         ),
       ),
     );
   }
 
   Widget _buildSignUpBtn() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        RichText(
-          text: TextSpan(
-            text: 'Don\'t have an Account? ',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18.0,
-              fontWeight: FontWeight.w400,
+    return Visibility(
+      visible: !_isSignup,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RichText(
+            text: TextSpan(
+              text: 'Don\'t have an Account? ',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w400,
+              ),
             ),
           ),
-        ),
-        TextButton(
-          onPressed: () => Get.to(() => SignUp()),
-          child: Text(
-            'Sign Up',
-            style: TextStyle(
-              fontSize: 18.0,
+          TextButton(
+            onPressed: () {
+              setState(() {
+                _isSignup = true;
+                _appBarTitle = 'Sign up';
+              });
+            },
+            child: Text(
+              'Sign Up',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTermsCheckbox() {
+    return Visibility(
+      visible: _isSignup,
+      child: Container(
+        height: 20.0,
+        child: Row(
+          children: <Widget>[
+            Theme(
+              data: ThemeData(unselectedWidgetColor: Colors.blue),
+              child: Checkbox(
+                value: _termsAndConditions,
+                checkColor: Colors.white,
+                onChanged: (value) {
+                  setState(() {
+                    _termsAndConditions = value!;
+                  });
+                },
+              ),
+            ),
+            RichText(
+                text: TextSpan(children: <TextSpan>[
+              TextSpan(
+                  text: "I agree to",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold)),
+              TextSpan(
+                  text: " terms and conditions",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                  recognizer: TapGestureRecognizer()..onTap = () {})
+            ])),
+            /*Text(
+              'I agree to',
+              style: kLabelStyle,
+            )*/
+          ],
         ),
-      ],
+      ),
+    );
+  }
+
+  Widget _buildConfirmPasswordTF() {
+    return Visibility(
+      visible: _isSignup,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 10.0),
+          Container(
+            alignment: Alignment.centerLeft,
+            height: 60.0,
+            child: TextFormField(
+              validator: (val) => Validator.isEqualTo(_password, val)
+                  ? null
+                  : "Password mis match",
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 1.0, color: Colors.blue)),
+                contentPadding: EdgeInsets.only(top: 14.0),
+                prefixIcon: Icon(
+                  Icons.lock,
+                ),
+                hintText: 'Confirm Password',
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -178,7 +344,7 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sign in'),
+        title: Text(_appBarTitle),
         centerTitle: true,
       ),
       body: Container(
@@ -196,9 +362,19 @@ class _SignInState extends State<SignIn> {
                 SizedBox(
                   height: 8.0,
                 ),
+                _buildConfirmPasswordTF(),
+                Visibility(
+                  visible: _isSignup,
+                  child: SizedBox(
+                    height: 8.0,
+                  ),
+                ),
                 _buildRememberMeCheckbox(),
                 _buildForgotPasswordBtn(),
                 _buildLoginBtn(),
+                _buildTermsCheckbox(),
+                _registerBtn(),
+                _buildSignInBtn(),
                 _buildSignUpBtn(),
               ],
             ),
