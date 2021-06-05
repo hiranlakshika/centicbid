117329ae6eaf9d49a031155631781b4c45e4b75f
Hiran Lakshika <hiranlakshika@gmail.com>
2021-06-05 13:07:57 +0530

    auction list design enhancements

--- lib/screens/auction_list_item.dart
+++ lib/screens/auction_list_item.dart
@@ -1,9 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:centicbid/models/auction.dart';
+import 'package:centicbid/theme/styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
 import 'package:get/get.dart';
 import 'item_details.dart';
+import 'package:getwidget/getwidget.dart';
 
 class AuctionListItem extends StatelessWidget {
   final Auction auction;
@@ -14,33 +18,56 @@ class AuctionListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 2.0,
-      child: Row(
-        children: [
-          CachedNetworkImage(
-            width: 100.0,
-            height: 100.0,
-            imageUrl: auction.imageUrl ?? 'http://via.placeholder.com/350x150',
-            progressIndicatorBuilder: (context, url, downloadProgress) =>
-                CircularProgressIndicator(value: downloadProgress.progress),
-            errorWidget: (context, url, error) => Icon(Icons.error),
-          ),
-          Column(
-            children: [
-              Text(auction.title),
-              Text(auction.description),
-              Text(auction.basePrice.toString()),
-              Text(auction.latestBid?.toString() ?? ''),
-              CountdownTimer(
-                endTime: DateTime.now().millisecondsSinceEpoch + 1000 * 10,
-                endWidget: Text('Expired'),
-              ),
-              ElevatedButton(
-                onPressed: () => Get.to(() => ItemDetails()),
-                child: Text('Bid Now'),
-              )
-            ],
-          ),
-        ],
+      child: GFListTile(
+        avatar: GFAvatar(
+          size: 70.0,
+          backgroundImage: CachedNetworkImageProvider(
+              auction.imageUrl ?? 'http://via.placeholder.com/350x150'),
+          shape: GFAvatarShape.standard,
+        ),
+        subTitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              auction.description,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text(
+              'Price :' + auction.basePrice.toString(),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text(
+              'Latest Bid :' + auction.latestBid!.toString(),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            CountdownTimer(
+              endTime: DateTime.now().millisecondsSinceEpoch + 1000 * 10,
+              endWidget: Text('Expired'),
+            )
+          ],
+        ),
+        title: Text(
+          auction.title,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: titleTextStyle,
+        ),
+        icon: ElevatedButton(
+          onPressed: () => Get.to(() => ItemDetails()),
+          child: Text('Bid Now'),
+        ),
       ),
     );
   }
---
+++ lib/theme/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+var titleTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 16.0,
+);
